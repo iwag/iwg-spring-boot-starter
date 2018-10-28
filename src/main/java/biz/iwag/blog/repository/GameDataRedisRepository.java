1dd0669ffba00da28da221d0b2c19b3c79a906a5
iwag <iwagame@gmail.com>
2018-10-27 18:39:25 -0700

    clear liquibase files

--- src/main/java/biz/iwag/blog/repository/GameDataRedisRepository.java
+++ src/main/java/biz/iwag/blog/repository/GameDataRedisRepository.java
@@ -1,19 +1,10 @@
 package biz.iwag.blog.repository;
 
 
-import biz.iwag.blog.config.Constants;
-import biz.iwag.blog.domain.GeneralSettings;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Repository
